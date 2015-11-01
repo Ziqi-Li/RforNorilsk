8ff7db3cfeb8f14363caaa7c8de086dbec24f3a2
Ziqi Li <liziqi1992@gmail.com>
2015-11-01 17:59:33 -0500

    update

--- major processing.R
+++ major processing.R
@@ -11,16 +11,22 @@ b = brick()
 b2=brick()
 for (i in (1:length(files))){
 	r = raster(files[i])
-	rdf = as.data.frame(r,xy=T)
-	colnames(rdf)=c("x","y","t")
-	rdf = rdf[complete.cases(rdf),]
-	model <- gstat(id = "t", formula = t~1, locations = ~x+y, data=rdf, 
-            nmax=7, set=list(idp = .5))
-	z <- interpolate(r, model)
-	z <- mask(z,)
-	b2=addLayer(b2, z)
+	#rdf = as.data.frame(r,xy=T)
+	#colnames(rdf)=c("x","y","t")
+	#rdf = rdf[complete.cases(rdf),]
+	#model <- gstat(id = "t", formula = t~1, locations = ~x+y, data=rdf, 
+            #nmax=7, set=list(idp = .5))
+	#z <- interpolate(r, model)
+	#z <- mask(z,)
+	#b2=addLayer(b2, z)
 	b=addLayer(b,r)
 }
+b1=brick("modis.grd")
+b1=stack(raster("merra_grid_no.tif"),b1)
+b2=brick("merra_ts.grd")
+b3=brick("merra_t2m.grd")
+b=brick(stack(b1,b2[[277:396]],b3[[277:396]]))
+
 
 df = as.data.frame(b,xy=T)
 i = 6
@@ -92,14 +98,61 @@ pl+geom_smooth(data=Molten,aes(x = year, y = temperature, group = class,color=cl
 
 
 
+#Merra
+merra_ts = k[,126:549]
+merra_t2m = k[,550:(ncol(k)-1)]
+merra_ts_0211 = merra_ts[,277:396]
+merra_t2m_0211 = merra_t2m[,277:396]
+modis_0211 = k[,6:125]
+
+#test with 29
+a = subset(df,merra_grid_no==30)
 
 
 
+p = cbind(a$x,a$y)
+for (j in (0:11)){
+	mts = a[,seq((127+j), (235+j), by=12)]
+	mt2m = a[,seq((247+j), (355+j), by=12)]
+	modis = a[,seq((7+j), (115+j), by=12)]
+	
+	res = summary(lm(t(modis[1,])~t(mt2m[1,])))$r.squared
+	for (i in (2:nrow(mt2m))){
+		if (!all(is.na(modis[i,])) & length(modis[i,])>=6){
+			temp = summary(lm(t(modis[i,])~t(mt2m[i,]),na.action=na.exclude))$r.squared
+			res = rbind(res,temp)
+		}
+		else{
+			res = rbind(res,c(NA))
+		}
+	}
+	p = cbind(p,res)
+}
 
 
 
+a = df
+p = cbind(a$x,a$y)
+j=3
+	mts = a[,seq((127+j), (235+j), by=12)]
+	mt2m = a[,seq((247+j), (355+j), by=12)]
+	modis = a[,seq((7+j), (115+j), by=12)]
+	res=c(NA,NA)
 
+	for (i in (1:nrow(mt2m))){
+		if (!all(is.na(modis[i,])) & !all(is.na(mt2m[i,]))){
+			temp = coef(lm(t(modis[i,])~t(mt2m[i,])))
+			res = rbind(res,temp)
+		}
+		else{
+			res = rbind(res,c(NA,NA))
+		}
+	}
+	p = cbind(p,res[2:dim(res)[1],])
 
+q = as.data.frame(b3,xy=T)
+q=cbind(q,p[,c(3,4)]
+q$new = q["MERRA_t2m_1981.4"]*q["t(mt2m[i, ])"]+q["(Intercept)"]
 
 
 
@@ -122,8 +175,8 @@ repeat{
 
 
 #rasteize
-spg <- a
+spg <- m
 coordinates(spg) <- ~ x + y
 gridded(spg) <- TRUE
-rasterDF <- raster(spg["temp"])
+rasterDF <- raster(spg["new"])
 plot(rasterDF)
